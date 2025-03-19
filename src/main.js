99fe4465d006a68d718c31a6a31af1fcaeb8c551
Moa Magnfors <moa@magnfors.se>
2025-03-19 19:13:18 +0100

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import "./css/style.css";
-import App from "./App.vue";
-import router from "./router";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "mapbox-gl/dist/mapbox-gl.css";
-
-createApp(App).use(router).mount("#app");
